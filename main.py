5baf94f2ad4db67f4a0db5797de869a73f08e5a6
mrcruz117 <mrcruz1989@gmail.com>
2022-06-20 16:49:46 -0400

    targeting weather conditions based on time and location

--- main.py
+++ main.py
@@ -7,9 +7,20 @@ QWM_endpoint = "https://api.openweathermap.org/data/2.5/onecall"
 weather_params = {
     "lat": lat,
     "lon": lon,
-    "appid": api_key
+    "appid": api_key,
+    "exclude": "current,minutely,daily"
 }
 
 response = requests.get(QWM_endpoint, params=weather_params)
-print()
-print(response.json())
+response.raise_for_status()
+
+weather_data = response.json()
+will_rain = False
+for i in range(12):
+    weather_code = weather_data["hourly"][i]["weather"][0]["id"]
+    if (weather_code) < 700:
+        will_rain = True
+        break
+
+if will_rain:
+    print("Bring Umbrella")
