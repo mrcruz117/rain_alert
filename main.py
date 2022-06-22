ca310beac90a3c405d991e1121e054b71e4b6d00
mrcruz117 <mrcruz1989@gmail.com>
2022-06-22 15:48:03 -0400

    automating process on pythonanywhere

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
-from env import api_key
+from twilio.rest import Client
+from env import api_key, twilio_SID, twilio_number, twilio_auth_token
 
 lat = 25.690142
 lon = -80.269402
@@ -16,11 +17,21 @@ response.raise_for_status()
 
 weather_data = response.json()
 will_rain = False
+
 for i in range(12):
     weather_code = weather_data["hourly"][i]["weather"][0]["id"]
-    if (weather_code) < 700:
+    print(weather_code)
+    if (weather_code) < 900:
         will_rain = True
         break
 
 if will_rain:
-    print("Bring Umbrella")
+    client = Client(twilio_SID, twilio_auth_token)
+
+    message = client.messages.create(
+        body="Remember to bring an umbrella! ☔",
+        from_=twilio_number,
+        to='+17862014285'
+    )
+
+print(message.status)
